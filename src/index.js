d35d800dff211f0a8c1f0eef5da8f054f54b9032
Sourabh Bansotiya <77042105+developer-sourabh28@users.noreply.github.com>
2024-05-23 17:41:15 +0000

    changes made

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { createRoot } from "react-dom/client";
 
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+const para =React.createElement(
+    "p",
+    {id: "abc"}, "I am learning React. My life is getting better."
+);
+const root = createRoot(document.getElementById("root"));
+root.render(para);
